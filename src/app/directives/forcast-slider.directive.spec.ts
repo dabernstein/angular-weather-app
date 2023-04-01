c6717736dd77c79b73e7d0b77642428ec0e8a3d6
Daniel Bernstein <Techdbernstein@gmail.com>
2023-03-31 23:01:59 -0400

    slider service changed to directive

---
+++ src/app/directives/forcast-slider.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ForcastSliderDirective } from './forcast-slider.directive';
+
+describe('ForcastSliderDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ForcastSliderDirective();
+    expect(directive).toBeTruthy();
+  });
+});
