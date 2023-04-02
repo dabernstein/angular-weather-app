53309387998c6c909dadf2b9ebaf7a29f2be91f1
Daniel Bernstein <turtletheft@outlook.com>
2023-04-02 18:13:38 -0400

    forecast base structure function

--- src/app/directives/forecast.directive.ts
+++ src/app/directives/forecast.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, EmbeddedViewRef, OnInit, Renderer2 } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Directive({
   selector: '[appForecast]'
 })
-export class ForecastDirective {
+export class ForecastDirective implements OnInit{
 
-  constructor() { }
+  constructor(private forecastContainer: ElementRef, private appData: AppComponent, private renderer: Renderer2) { }
+
+  ngOnInit() {
+
+  }
+
+  createForecastBlocks(time: string, temp: string) {
+    const outerElem: HTMLElement = document.createElement('div');
+    outerElem.classList.add('hour-info');
+    const innerTopElem: HTMLElement = document.createElement('div');
+    innerTopElem.classList.add('hour');
+    const innerBottomElem: HTMLElement = document.createElement('div');
+    innerBottomElem.classList.add('hour-temp');
+
+    innerTopElem.textContent = time;
+    innerBottomElem.textContent = temp + "°F";
+
+    this.forecastContainer.nativeElement.appendChild(outerElem);
+    outerElem.appendChild(innerTopElem);
+    outerElem.appendChild(innerBottomElem);
+  }
 
 }
